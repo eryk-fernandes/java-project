dca6d00a5c7498ff269039d83e4379f3d2dbb632
Eryk Fernandes <erykfernandespro@gmail.com>
2025-02-06 10:12:11 -0300

    working with packages

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,16 @@
+package application;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Age: ");
+        double age = scanner.nextDouble();
+
+        System.out.println("You are " + age + " years old");
 
         scanner.close();
     }
